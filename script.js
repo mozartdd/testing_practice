d336dc422851427a15cca484e7fe6a2571d2fbc1
Dainis Dilevka <thedenisovan@proton.me>
2025-07-21 08:59:40 +0100

    Added calculator funciton and its test

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 export function capitalize(str) {
+  if (typeof str !== 'string') {
+    throw new Error('Please enter valid string input.');
+  }
+
   const firstLetter = str.slice(0,1).toUpperCase();
   const strTail = str.slice (1);
   return firstLetter + strTail;
 }
 
 export function reverseString(str) {
+  if (typeof str !== 'string') {
+    throw new Error('Please enter valid string input.');
+  }
+
   return str.split('').reverse('').join('');
+}
+
+// Helper function to validate input for calculator obj functions
+function validateNumbers(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('Both input values must be numbers.');
+  }
+};
+
+export const calculator = {
+  add: (a, b) => {
+    validateNumbers(a, b);
+    return a + b;
+  },
+  subtract: (a, b) => {
+    validateNumbers(a, b);
+    return a - b;
+  },
+  divide: (a, b) => {
+    validateNumbers(a, b);
+    return Number((a / b).toFixed(2));
+  },
+  multiply: (a, b) => {
+    validateNumbers(a, b);
+    return a * b;
+  }
 }
--- script.test.js
+++ script.test.js
@@ -1,8 +1,31 @@
-import { capitalize, reverseString } from "./script";
+import { capitalize, reverseString, calculator } from "./script";
 
+// Test for capitalize function
 test('Expects parameter input "jack" to return "Jack"', () => {
   expect(capitalize('jack')).toBe('Jack');
 });
+test('Expects parameter input "Adam" to return "Adam"', () => {
+  expect(capitalize('Adam')).toBe('Adam');
+});
+
+// Test for reverse string function
 test('Expect parameter input John to return nhoJ', () => {
   expect(reverseString('John')).toBe('nhoJ');
 });
+test('Expect parameter input aka car to return aka', () => {
+  expect(reverseString('aka')).toBe('aka');
+});
+
+// Test for mathematical functions
+test('2 + 2 = 4', () => {
+  expect(calculator.add(2, 2)).toEqual(4);
+});
+test('7 - 3 = 4', () => {
+  expect(calculator.subtract(7, 3)).toEqual(4);
+});
+test('5 / 2 = 2.50', () => {
+  expect(calculator.divide(5, 2)).toBeCloseTo(2.5);
+})
+test('5 * 5 = 25', () => {
+  expect(calculator.multiply(5, 5)).toEqual(25);
+})
